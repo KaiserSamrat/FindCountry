676f9cc5fdc1f9fff96394e70f5affc4c5e85d44
Kaiser Samrat <kaisersamrat136@gmail.com>
2021-05-29 23:16:50 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./containers/Header"
+import FoodList from "./containers/FoodList"
+import FoodDetails from "./containers/FoodDetails";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' >
+            <FoodList />
+          </Route>
+          <Route path='/food/:foodId'>
+            <FoodDetails></FoodDetails>
+
+          </Route>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
